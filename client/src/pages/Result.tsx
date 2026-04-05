b99833e332ce7d1d990f54191002e6e5628dcd2a
kartik Saini <kartiksainisaini165@gmail.com>
2026-04-05 13:49:08 +0530

    progress+

--- client/src/pages/Result.tsx
+++ client/src/pages/Result.tsx
@@ -1,8 +1,42 @@
+import { useEffect, useState } from "react"
+import { Project } from "../types"
+import { dummyGenerations } from "../assets/assets"
+import { Loader2Icon, RefreshCwIcon } from "lucide-react"
+import { Link } from "react-router-dom"
 
 const Result = () => {
-  return (
-    <div>
-      
+
+  const [project, setprojectData] = useState<Project>({} as Project)
+  const [loading, setLoading] = useState(true)
+  const [isGenerating, setIsGenerating] = useState(true)
+
+
+  const fetchProjectData = async ()=>{
+    setTimeout(()=>{
+      setprojectData(dummyGenerations[0])
+      setLoading(false)
+    }, 2000)
+  }
+  
+  useEffect(()=>{
+    fetchProjectData()
+  },[])
+
+  return loading ? (
+    <div className="h-screen w-full flex items-center justify-center">
+      <Loader2Icon className="animate-spin text-indigo- size-9" />
+    </div>
+  ) : (
+    <div className="min-h-screen text-white p-6 md:p-12 mt-20">'
+    <div className="max-w-6xl mx-auto">
+      <header className="flex justify-between items-center mb-8">
+        <h1>Generation Result</h1>
+        <Link to="/generate" className="btn-secondary text-sm flex items-center gap-2">
+        <RefreshCwIcon className="w-4 h-4" />
+        <p className="max-sm:hidden">New Generation</p>
+        </Link>
+      </header>
+    </div>
     </div>
   )
 }
